a733a3615323c6441291977e62fbda20f5add496
Enner Pérez <ennerperez@gmail.com>
2019-04-04 13:41:20 -0500

    Initial development

---
+++ src/FloatingActionButton/Models/FloatingAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.AspNetCore.Mvc.Models
+{
+    public class FloatingAction
+    {
+        public List<FloatingAction> Actions { get; set; }
+
+        public string Title { get; set; }
+
+        public string Class { get; set; } = "btn-default";
+
+        public string Url { get; set; }
+        public string Target { get; set; }
+        public string Icon { get; set; }
+
+        public FloatingAction()
+        {
+            Actions = new List<FloatingAction>();
+        }
+
+    }
+}
